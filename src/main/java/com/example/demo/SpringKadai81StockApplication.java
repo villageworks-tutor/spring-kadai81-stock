54029c84c9c2d8391a65188a019634e577d34355
villageworks <villageworks@gmail.com>
2023-12-08 16:55:06 +0900

    ver.0.00: プロジェクト初版

---
+++ src/main/java/com/example/demo/SpringKadai81StockApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKadai81StockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKadai81StockApplication.class, args);
+	}
+
+}
